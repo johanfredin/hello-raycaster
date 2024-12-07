f4dea2b50849ce03d1f7a6a0f4b2deee57170262
Johan Fredin <johan.fredin.2@gu.se>
2024-12-07 20:30:18 +0100

    collision detection basic

--- js-prototyping/raycast.js
+++ js-prototyping/raycast.js
@@ -33,20 +33,101 @@ class Map {
             }
         }
     }
+
+    hasWallAt(x, y) {
+        if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT) {
+            return true;
+        }
+
+        const gridX = Math.floor(x / TILE_SIZE);
+        const gridY = Math.floor(y / TILE_SIZE);
+        return this.grid[gridY][gridX] == 1;
+    }
+}
+
+class Player {
+    constructor() {
+        this.x = WINDOW_WIDTH / 2;
+        this.y = WINDOW_HEIGHT / 2;
+        this.radius = 3;        // Size of player
+        this.turnDirection = 0; // -1 if left or +1 if right
+        this.walkDirection = 0; // -1 if back of +1 if front
+        this.rotationAngle = Math.PI / 2;
+        this.moveSpeed = 2.0;
+        this.rotationSpeed = 2 * (Math.PI / 180);
+    }
+
+    render() {
+        noStroke();
+        fill("red");
+        circle(this.x, this.y, this.radius);
+        stroke("red");
+        line(
+            this.x,
+            this.y,
+            this.x + Math.cos(this.rotationAngle) * 20,
+            this.y + Math.sin(this.rotationAngle) * 20,
+        )
+    }
+
+    update() {
+        this.rotationAngle += this.turnDirection * this.rotationSpeed;
+        const moveStep = this.walkDirection * this.moveSpeed;
+
+        // Calculate what next x and y will be 
+        const newPlayerX = this.x + Math.cos(this.rotationAngle) * moveStep;
+        const newPlayerY = this.y + Math.sin(this.rotationAngle) * moveStep;
+
+        // Update position if we are not colliding with a wall
+        if (!grid.hasWallAt(newPlayerX, newPlayerY)) {
+            this.x = newPlayerX;
+            this.y = newPlayerY;
+        }
+    }
 }
 
 const grid = new Map();
+const player = new Player();
+
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        player.walkDirection = 1;
+    } else if (keyCode == DOWN_ARROW) {
+        player.walkDirection = -1;
+    }
+
+    if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = 1;
+    } else if (keyCode == LEFT_ARROW) {
+        player.turnDirection = -1;
+    }   
+}
+
+function keyReleased() {
+    if (keyCode == UP_ARROW) {
+        player.walkDirection = 0;
+    } else if (keyCode == DOWN_ARROW) {
+        player.walkDirection = 0;
+    } 
+    
+    if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = 0;
+    } else if (keyCode == LEFT_ARROW) {
+        player.turnDirection = 0;
+    }   
+}
 
 function setup() {
     createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 function update() {
-    // TODO: update all game objects before we render the next frame
+    player.update();
 }
 
 function draw() {
     update();
 
     grid.render();
+    player.render();
 }
